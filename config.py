59151cee265f5577dca51af967dac6a09bb9bbd4
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-06-29 21:44:20 +0530

    Update

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ import os
 class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
- SOURCE="https://github.com/Artis7eeR/ForwardTagRemoverBot"
+ SOURCE="https://t.me/doreamonfans1"
  START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©disneygrou"
  HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 	
