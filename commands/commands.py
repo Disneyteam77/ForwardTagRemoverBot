953db301b496db1560e59865534a6c00b11bb42a
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-06-29 21:45:35 +0530

    Update

--- commands/commands.py
+++ commands/commands.py
@@ -19,10 +19,10 @@ from config import Config
 #Inline Keyboard Button
 keyboard = [
 [
- InlineKeyboardButton("Source Code", url=Config.SOURCE)
+ InlineKeyboardButton("Support", url=Config.SOURCE)
 ],
 [
- InlineKeyboardButton("How To Create A Bot Like Me",url="https://youtu.be/swg6un2N4Fk")
+ InlineKeyboardButton("Updates",url="https://t.me/disneygrou")
 ]
 ]
 
